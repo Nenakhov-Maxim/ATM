8e8a99e3e65e3df34c9e6b1ea56ede0bb82d3097
Nenakhov-Maxim <Nenakhov.Max@yandex.ru>
2026-03-04 11:46:11 +0500

    Перенос в worker.js в отдельную папку

--- app/static/js/worker.js
+++
@@ -1,5 +0,0 @@
-// Worker entrypoint.
-// Functional code is split into:
-// - worker-api.js
-// - worker-ui.js
-// - worker-websocket.js
---
+++ app/static/js/worker/worker.js
@@ -0,0 +1,5 @@
+// Worker entrypoint.
+// Functional code is split into:
+// - worker/worker-api.js
+// - worker/worker-ui.js
+// - worker/worker-websocket.js
